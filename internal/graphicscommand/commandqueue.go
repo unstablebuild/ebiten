c5716a26818461e7a41e115496bc8ba382d21f52
ernestrc <ernest@unstable.build>
2024-07-02 16:50:44 -0700

    Update graphicscommand.commandQueue.flush to remove one uneccessary allocation
    Defer statement with closure calls mallocvia runtime.newobject

--- internal/graphicscommand/commandqueue.go
+++ internal/graphicscommand/commandqueue.go
@@ -222,6 +222,40 @@ func (q *commandQueue) Flush(graphicsDriver graphicsdriver.Graphics, endFrame bo
 	return nil
 }
 
+func (q *commandQueue) endFlush(
+	graphicsDriver graphicsdriver.Graphics, endFrame bool,
+) (err error) {
+	// Call End even if an error causes, or the graphics driver's state might be stale (#2388).
+	if err1 := graphicsDriver.End(endFrame); err1 != nil && err == nil {
+		err = err1
+	}
+
+	// Release the commands explicitly (#1803).
+	// Apparently, the part of a slice between len and cap-1 still holds references.
+	// Then, resetting the length by [:0] doesn't release the references.
+	for i, c := range q.commands {
+		if c, ok := c.(*drawTrianglesCommand); ok {
+			q.drawTrianglesCommandPool.put(c)
+		}
+		q.commands[i] = nil
+	}
+	q.commands = q.commands[:0]
+	q.vertices = q.vertices[:0]
+	q.indices = q.indices[:0]
+	q.tmpNumVertexFloats = 0
+
+	if endFrame {
+		q.uint32sBuffer.reset()
+		for i, f := range q.finalizers {
+			f()
+			q.finalizers[i] = nil
+		}
+		q.finalizers = q.finalizers[:0]
+	}
+
+	return err
+}
+
 // flush must be called the render thread.
 func (q *commandQueue) flush(graphicsDriver graphicsdriver.Graphics, endFrame bool, logger debug.Logger) (err error) {
 	// If endFrame is true, Begin/End should be called to ensure the framebuffer is swapped.
@@ -236,36 +270,6 @@ func (q *commandQueue) flush(graphicsDriver graphicsdriver.Graphics, endFrame bo
 	if err := graphicsDriver.Begin(); err != nil {
 		return err
 	}
-
-	defer func() {
-		// Call End even if an error causes, or the graphics driver's state might be stale (#2388).
-		if err1 := graphicsDriver.End(endFrame); err1 != nil && err == nil {
-			err = err1
-		}
-
-		// Release the commands explicitly (#1803).
-		// Apparently, the part of a slice between len and cap-1 still holds references.
-		// Then, resetting the length by [:0] doesn't release the references.
-		for i, c := range q.commands {
-			if c, ok := c.(*drawTrianglesCommand); ok {
-				q.drawTrianglesCommandPool.put(c)
-			}
-			q.commands[i] = nil
-		}
-		q.commands = q.commands[:0]
-		q.vertices = q.vertices[:0]
-		q.indices = q.indices[:0]
-		q.tmpNumVertexFloats = 0
-
-		if endFrame {
-			q.uint32sBuffer.reset()
-			for i, f := range q.finalizers {
-				f()
-				q.finalizers[i] = nil
-			}
-			q.finalizers = q.finalizers[:0]
-		}
-	}()
 
 	cs := q.commands
 	for len(cs) > 0 {
@@ -284,6 +288,7 @@ func (q *commandQueue) flush(graphicsDriver graphicsdriver.Graphics, endFrame bo
 		}
 		if 0 < ne {
 			if err := graphicsDriver.SetVertices(vs[:nv], es[:ne]); err != nil {
+				_ = q.endFlush(graphicsDriver, endFrame)
 				return err
 			}
 			es = es[ne:]
@@ -292,6 +297,7 @@ func (q *commandQueue) flush(graphicsDriver graphicsdriver.Graphics, endFrame bo
 		indexOffset := 0
 		for _, c := range cs[:nc] {
 			if err := c.Exec(q, graphicsDriver, indexOffset); err != nil {
+				_ = q.endFlush(graphicsDriver, endFrame)
 				return err
 			}
 			logger.Logf("  %s\n", c)
@@ -305,7 +311,7 @@ func (q *commandQueue) flush(graphicsDriver graphicsdriver.Graphics, endFrame bo
 		cs = cs[nc:]
 	}
 
-	return nil
+	return q.endFlush(graphicsDriver, endFrame)
 }
 
 type rectangleF32 struct {
